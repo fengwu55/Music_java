8f514e46fa43acee84c5bf44f4fc709c0dd37579
feng <2370739533@qq.com>
2022-11-04 20:05:04 +0800

    :art:  基本架构

---
+++ music_java/src/main/java/com/music/music_java/service/UserService.java
@@ -0,0 +1,18 @@
+package com.music.music_java.service;
+
+import com.music.music_java.entity.mbg.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.music.music_java.entity.vo.UserVo;
+
+/**
+ * <p>
+ * 用户 服务类
+ * </p>
+ *
+ * @since 2022-11-03
+ */
+public interface UserService {
+
+    User login(UserVo userVo);
+
+}
